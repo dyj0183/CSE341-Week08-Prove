b2954d624ecbd89c444e7cec832725343f82a18e
Yu-Chun <dyj0183@byui.edu>
2021-06-16 23:37:31 -0600

    tried to update CSS

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 // import products route
@@ -6,6 +7,9 @@ const route = require("./routes/products");
 
 app.set('view engine', 'ejs'); // set up the template engine ejs
 app.set('views', 'views'); // let the program know where to find the templates
+
+//register a static folder, so that we can use the css files directly from HTML pages in our public folder 
+app.use(express.static(path.join(__dirname, 'public')));
 
 // use the route
 app.use(route);
